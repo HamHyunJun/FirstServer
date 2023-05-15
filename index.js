5190fcc05e5bddbc07e208982027fa1dde934971
HamHyunJun <hhj3667@iob.kr>
2023-05-15 10:21:42 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://hhj3667:gkaguswns123!@ham.9nx8pnk.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(()=> console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => res.send('안녕하세요'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
